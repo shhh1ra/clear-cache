92815aa0a832f36f0e9c5e8b0d3a8bb56d4c137d
shira <85801159+shhh1ra@users.noreply.github.com>
2025-11-18 19:32:36 +0400

    added zapret downloads

---
+++ zp-download.bat
@@ -0,0 +1,4 @@
+del "zapret-*"
+curl -L "https://github.com/Flowseal/zapret-discord-youtube/releases/download/1.9.0b/zapret-discord-youtube-1.9.0b.zip" -O "zapret-discord-youtube-1.9.0b.zip"
+cd /d D:
+tar -xvf zapret-discord-youtube-1.9.0b.zip && del "zapret-*.zip"
