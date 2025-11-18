9ca85f3cd67bd5097ec07fc5fcef8cb65ed1ac61
shira <85801159+shhh1ra@users.noreply.github.com>
2025-11-18 20:00:49 +0400

    Update zp-download.bat

--- zp-download.bat
+++ zp-download.bat
@@ -1,5 +1,7 @@
 del "zapret-*"
+mkdir "zapret-latest"
 curl -L "https://github.com/Flowseal/zapret-discord-youtube/releases/download/1.9.0b/zapret-discord-youtube-1.9.0b.zip" -O "zapret-discord-youtube-1.9.0b.zip"
-cd /d D:
-mkdir zapret-latest
-tar -xvf zapret-discord-youtube-1.9.0b.zip -C "D:\zapret-latest" && del "zapret-*.zip"
+move "D:\zapret-discord-youtube-1.9.0b.zip" "D:\zapret-latest\zapret-discord-youtube-1.9.0b.zip"
+cd zapret-latest
+tar -xvf zapret-discord-youtube-1.9.0b.zip
+del "zapret-*.zip"
